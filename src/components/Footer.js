c9395aa7f07bc024f20989fc4184f90b9c0c383b
github username <github email address>
2020-01-09 18:37:33 -0700

    added forms and styling to front end

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer(props){
+    return(
+        <>
+            <h1 className='footer-items'>Social Media For Alumni</h1>
+            <a href='https://github.com/adampaez88' className='footer-items'>GitHub</a>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/Post.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'font-awesome/css/font-awesome.min.css'
+
+
+export default function Post({post}) {
+    return (
+        <div className='a-post' >
+            <img src={post.image_url} alt='some photo'/>
+            <div className='post-buttons'>
+                <button>likes:{post.like}</button> 
+                <button>dislikes: {post.dislike}</button>
+            </div>
+
+            <h1>{post.content}</h1>
+        </div>
+    )
+}
