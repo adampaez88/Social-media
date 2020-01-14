eae2662a4ae0cc49aa5cfaf6c788d5c8b98315e0
github username <github email address>
2020-01-14 13:09:57 -0700

    fixed delete button

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -50,8 +50,12 @@ export default class Comments extends Component{
 
     // likeClick = () => {
     //     this.state.comments.map(comment => {
-    //         let count = comment.like
-    //         return count += 1
+    //         let count = comment.like += 1
+    //         fetch(`http://localhost:8000/comments/${comment.id}`, {
+    //             method: 'POST',
+    //             headers: {'Content-Type': 'application/json'},
+    //             body: JSON.stringify({'like': count})
+    //         })
     //     })
     // }
 
@@ -62,14 +66,31 @@ export default class Comments extends Component{
         })
     }
 
+    deleteClick = (id) => {
+        const urComment = this.state.comments.find(comment => {
+            return comment.id === id
+        })
+        if (this.props.post.user_id === urComment.user_id){
+            const comments = this.state.comments.filter(comment => {
+                return comment.id !== id
+            })
+            this.setState({
+                comments
+            })  
+            fetch(`http://localhost:8000/comments/${id}`, {
+                method: 'DELETE'
+            })
+        }
+    }
+
     eachComment = () => {
         return this.filterComments().map(comment => {
             return( 
                 <div className='comment-list'>
                  <li className='a-comment'>{comment.content}</li>
                  <div>
-                    <button>Like: {comment.like}</button>
-                    <button>Delete</button>
+                    <button>Likes: {comment.like}</button>
+                    <button onClick={() => this.deleteClick(comment.id)}>Delete</button>
                  </div>
                </div>
             )
@@ -77,28 +98,25 @@ export default class Comments extends Component{
     }
     
     render(){
-        // const {comments, show, handleClick} = this.props
         const {show, handleClick} = this.props
-       
         return (
             <div className='comments-div'>
     
-                <button onClick={handleClick}>Add Comment</button>
+                <button onClick={handleClick}>Comments</button>
                 <form style={{visibility: show ? "visible" : "hidden"}} 
                     onSubmit={this.handleSubmit}
                 >
                     <input
                         type='text' value={this.state.content}
-                        name='content' placeholder='Comment'
+                        name='content' placeholder='Add Comment'
                         onChange={this.handleChange} required
                     />
                     <input type='submit' className='comment-submit'/>
                 </form>
-                <ul>
+                <ul style={{display: show ? 'block' : 'none'}}>
                     {this.eachComment()}
                 </ul>
             </div>
         )
     }
-   
 }
